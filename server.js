d0fd680c1c92aa6527c45aadf19b29200c0c8cd0
Dhanushya5642 <188843845+Dhanushya5642@users.noreply.github.com>
2026-06-11 14:59:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://amazing-valkyrie-bbc067.netlify.app",
+      "https://bodega-grocery.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
